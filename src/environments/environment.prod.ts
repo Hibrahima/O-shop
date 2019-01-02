2df0eccf55ae2411dbd7c6dd400f64132ba10ac6
Hibrahima <haidara99@gmail.com>
2019-01-02 17:15:01 +0100

    - added firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBSM_wBxA5D5YK4FCzweulXM59D2bEtKmU",
+    authDomain: "oshop-469aa.firebaseapp.com",
+    databaseURL: "https://oshop-469aa.firebaseio.com",
+    projectId: "oshop-469aa",
+    storageBucket: "oshop-469aa.appspot.com",
+    messagingSenderId: "590456096197"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBSM_wBxA5D5YK4FCzweulXM59D2bEtKmU",
+    authDomain: "oshop-469aa.firebaseapp.com",
+    databaseURL: "https://oshop-469aa.firebaseio.com",
+    projectId: "oshop-469aa",
+    storageBucket: "oshop-469aa.appspot.com",
+    messagingSenderId: "590456096197"
+  }
 };
 
 /*
