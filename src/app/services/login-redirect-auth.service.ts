3cc57e29315bf64d5c6640db3c63364c60eb7a90
Hibrahima <haidara99@gmail.com>
2019-01-04 23:57:10 +0100

    - saved user when logged in successfully

--- src/app/services/login-redirect-auth.service.ts
+++ src/app/services/login-redirect-auth.service.ts
@@ -1,3 +1,4 @@
+import { UserService } from './user.service';
 import { CanActivate } from '@angular/router/';
 import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
@@ -8,12 +9,17 @@ import { Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/ro
 })
 export class LoginRedirectAuthGuard implements CanActivate{
 
-  constructor(private authSerice: AuthService, private router: Router) { }
+  constructor(private authSerice: AuthService, private router: Router, private userService: UserService) { }
 
   canActivate( route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     
    return this.authSerice.afAuth.auth.getRedirectResult().then(result => {
       if(result.user != null) {
+        /* there is a user logged in, so redirects to the origin url
+           and save this user 
+        */
+
+        this.userService.save(result.user);
         this.router.navigateByUrl(localStorage.getItem('returnUrl'));
         return false;
       }
