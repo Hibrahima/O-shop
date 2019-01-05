91a4a95ca1181b9f573e1d0c2f59e59b3193a675
Hibrahima <haidara99@gmail.com>
2019-01-05 22:24:17 +0100

    - subscribed to productService.getAll() to initialize filteredProducts and products fields - implemented OnDestroy to unsubscribe - added delete product functionality - added filter functionality

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,20 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductService } from 'src/app/services/product.service';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
+export class AdminProductsComponent implements OnInit, OnDestroy {
 
-  products$;
+  filteredProducts: any[];
+  products: {name: string}[];
+  subscription: Subscription;
 
-  constructor(private productService: ProductService) { 
-    this.products$ = productService.getAll();
+  constructor(private productService: ProductService, private router: Router) { 
+    this.subscription = productService.getAll()
+    .subscribe(products => this.filteredProducts = this.products = products);
   }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
+  }
+
+  deleteProduct(id: string){
+    if(!confirm('Are you sure you want to delete this product?')) return;
+
+    this.productService.delete(id);
+    this.router.navigateByUrl('/admin/products');
+  }
+
+  filter(query: string){
+    // if the query is not null and empty, filters and resets the filteredProducts field
+    // filtering is done with lowercase 
+    // if the query is null or an empty string, just sets filteredProducts to products (all from the database) 
+    this.filteredProducts = (query) ? 
+    this.products.filter(p => p.name.toLowerCase().includes(query.toLowerCase())) :
+    this.products;
   }
 
 }
