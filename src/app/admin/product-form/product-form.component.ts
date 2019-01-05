565ab0b844cf06f90e8f9d3dea591b1b08a95c40
Hibrahima <haidara99@gmail.com>
2019-01-05 22:31:39 +0100

    - added product field and initialized it to an empty object - added productId field to hold the id of the product to update - changed save method to either create a new product or update an existing one based on the productId field

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,7 +1,9 @@
+import { ActivatedRoute } from '@angular/router/';
 import { ProductService } from './../../services/product.service';
 import { CategoryService } from './../../services/category.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-product-form',
@@ -11,19 +13,37 @@ import { Router } from '@angular/router';
 export class ProductFormComponent implements OnInit {
 
   categories$;
+  product = {};
+  productId;
 
   constructor(
     categoryService: CategoryService, 
     private productService: ProductService,
-    private router: Router) { 
+    private router: Router,
+    private route: ActivatedRoute) { 
     this.categories$ = categoryService.getAll();
+    
+    // gets the id query param from the activated route
+    this.productId = route.snapshot.paramMap.get('id');
+
+    /** gets the related product from the database if id is not null
+        and initializes the product field
+        take operator will automatically unsubscribe when the subscribe operation is complete
+    **/
+    if(this.productId) 
+       productService.get(this.productId).pipe(take(1)).subscribe(p => this.product = p);
   }
 
   ngOnInit() {
   }
 
   save(product){
-    this.productService.create(product);
+    // if the product id is not null, we are updating, so updates the related product
+    // if not, then create a new product
+    if(this.productId)
+      this.productService.update(this.productId, product);
+    else
+      this.productService.create(product);
     this.router.navigateByUrl('/admin/products');
   }
 
