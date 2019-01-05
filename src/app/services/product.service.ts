0eab6fc64807e1a1799735f0363e7778e6fc86d0
Hibrahima <haidara99@gmail.com>
2019-01-05 16:30:50 +0100

    - added itemsRef field - added item field - added dbNodeName field - refactored code - change implementation of create(p) to use the itemsRef - added getAll() method

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,14 +1,30 @@
-import { AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor(private db: AngularFireDatabase) { }
+  itemsRef: AngularFireList<any>;
+  items: Observable<any[]>;
+  dbNodeName: string = '/products';
 
-  create(product){
-    return this.db.list('/products').push(product);
+  constructor(db: AngularFireDatabase) {
+    this.itemsRef = db.list(this.dbNodeName);
+   }
+
+  create(product) {
+    return this.itemsRef.push(product);
+  }
+
+  getAll(): Observable<any[]> {
+    return this.itemsRef.snapshotChanges().pipe(
+      map(changes => 
+        changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
+      )
+    );
   }
 }
