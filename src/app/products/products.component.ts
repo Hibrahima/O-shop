835233c9204f4d0f4d7b66ef5419f8a6a930532d
Hibrahima <haidara99@gmail.com>
2019-01-06 21:10:46 +0100

    - added filtering logic

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,9 @@
+import { switchMap } from 'rxjs/operators';
+import { Product } from './../models/product';
+import { ActivatedRoute, Router } from '@angular/router/';
+import { ProductService } from './../services/product.service';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-products',
@@ -7,7 +12,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+  products: Product[] = [];
+  filteredProducts: Product[] = [];
+  activatedRoute: string;
+  selectedCategory: string;
+  cardWidth: string = '15 px'; // TO DO
+
+  constructor(productService: ProductService, route:ActivatedRoute) {
+
+    productService.getAll().pipe(switchMap(products => {
+      this.products = products;
+      return route.queryParamMap;
+    }))
+    .subscribe(params => {
+        this.selectedCategory = params.get('category');
+        this.filteredProducts = (this.selectedCategory) ? 
+        this.products.filter(p => p.category === this.selectedCategory) :
+        this.products;
+      });
+
+    //this.activatedRoute = router.url;
+    //console.log(" activated url = "+this.activatedRoute);
+
+   }
 
   ngOnInit() {
   }
