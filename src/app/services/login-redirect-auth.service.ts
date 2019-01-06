47670d1f9789d47d488d8fca23354cd28ef29918
Hibrahima <haidara99@gmail.com>
2019-01-06 19:22:19 +0100

    - refactored code

--- src/app/services/login-redirect-auth.service.ts
+++ src/app/services/login-redirect-auth.service.ts
@@ -7,23 +7,28 @@ import { Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/ro
 @Injectable({
   providedIn: 'root'
 })
-export class LoginRedirectAuthGuard implements CanActivate{
+export class LoginRedirectAuthGuard implements CanActivate {
 
   constructor(private authSerice: AuthService, private router: Router, private userService: UserService) { }
 
-  canActivate( route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    
-   return this.authSerice.afAuth.auth.getRedirectResult().then(result => {
-      if(result.user != null) {
-        /* there is a user logged in, so redirects to the origin url
-           and save this user 
-        */
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
 
-        this.userService.save(result.user);
-        this.router.navigateByUrl(localStorage.getItem('returnUrl'));
-        return false;
-      }
-      return true;
+    return this.authSerice.afAuth.auth.getRedirectResult().then(result => {
+      // there is no logged in user, so return true so that this route can be activated
+      if (!result.user) return true;
+
+      /* there is a user logged in, so redirects to the origin url
+         and save this user 
+       */
+
+      this.userService.save(result.user);
+      let returnUrl = localStorage.getItem('returnUrl');
+      if (!returnUrl) return;
+
+      // removes the return url from the local storage
+      localStorage.removeItem('returnUrl');
+      this.router.navigateByUrl(returnUrl);
+      return false;
     });
   }
 }
