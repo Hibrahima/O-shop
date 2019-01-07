eed1319957786c51c3790c2d0ee39cdc6301b761
Hibrahima <haidara99@gmail.com>
2019-01-07 21:17:25 +0100

    - added shopping car total items count field

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,19 +1,27 @@
+import { ShoppingCartService } from './../services/shopping-cart.service';
 import { AppUser } from './../models/app-user';
 import { AuthService } from '../services/auth.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ShoppingCart } from '../models/shopping-cart';
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent  {
+export class BsNavbarComponent implements OnInit {
 
   appUser : AppUser;
+  shoppingCartItemsCount;
+  cart$: Observable<ShoppingCart>;
 
-  constructor(private auth : AuthService) { 
-    auth.appUser$.subscribe(user => this.appUser = user);
-   }
+  constructor(private auth : AuthService, private cartService: ShoppingCartService) { }
+
+  async ngOnInit(){
+    this.auth.appUser$.subscribe(user => this.appUser = user);
+    this.cart$ = await this.cartService.getCart();
+  }
 
   logout(){
     this.auth.logout();
