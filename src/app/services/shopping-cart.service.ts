2f02fa6e28ebef09a28de85ac04c6150836c8da9
Hibrahima <haidara99@gmail.com>
2019-01-07 21:41:08 +0100

    - refactored code

--- src/app/services/shopping-cart.service.ts
+++ src/app/services/shopping-cart.service.ts
@@ -1,8 +1,9 @@
 import { AngularFireDatabase, AngularFireList, AngularFireObject } from '@angular/fire/database';
 import { Injectable } from '@angular/core';
 import { Product } from '../models/product';
-import { take } from 'rxjs/operators';
+import { take, map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
+import { ShoppingCart } from '../models/shopping-cart';
 
 @Injectable({
   providedIn: 'root'
@@ -22,7 +23,24 @@ export class ShoppingCartService {
     });
   }
 
-  private async getOrCreateCartId(){
+  async getCart(): Promise<Observable<ShoppingCart>>{
+    let cartId: string = await this.getOrCreateCartId();
+    let cart$ = this.db.object(this.dbNodeName + '/' + cartId).valueChanges() as Observable<ShoppingCart>;
+    return cart$.pipe(map(cart => new ShoppingCart(cart.items)));
+    
+  }
+
+  async addToCart(product: Product){
+    this.updateItemQuantity(product, 1);
+  }
+
+  // does not really remove, just decrements the quantity by 1
+  async removeFromCart(product: Product){
+    this.updateItemQuantity(product, -1);
+  }
+
+
+  private async getOrCreateCartId(): Promise<string>{
     let cartId = localStorage.getItem('cartId');
     if(cartId) return cartId;
 
@@ -33,23 +51,22 @@ export class ShoppingCartService {
    
   }
 
-  getCart(cardId: string){
-    return this.db.object(this.dbNodeName + '/' + cardId).valueChanges();
-  }
-
   private getItem(cartId: string, productKey: string): AngularFireObject<any>{
      return this.db.object(this.dbNodeName + '/' + cartId + '/items/' + productKey);
   }
 
-  async addToCart(product: Product){
+  private async updateItemQuantity(product: Product, change: number){
     let cartId = await this.getOrCreateCartId();
     let productRef: AngularFireObject<any> = this.getItem(cartId, product.key);
     let item$ = productRef.valueChanges();
     item$.pipe(take(1)).subscribe(item => {
+
       // persists a new object which contains a product and
-      // sets its quantity to (0 + 1 = 1) if the product is not in any shopping cart
+      // sets its quantity to 1 if the product is not in any shopping cart
       // or sets its quantity to the quantity value stored in the database plus 1
-      productRef.update({product: product, quantity: ( item.quantity || 0) + 1 });
-    })
+      if(item) productRef.update({quantity: item.quantity  + change });
+      else productRef.update({product: product, quantity: 1});
+    });
   }
+ 
 }
