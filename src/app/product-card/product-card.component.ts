37fc44eb34aaa1a4a98cb126738c0a7abf639bf5
Hibrahima <haidara99@gmail.com>
2019-01-07 21:21:28 +0100

    - changed addToCart() method signature - removed parameter product from addToCart() method - added shopping-cart input property field - added decrementQuantity() method - added getQuantity() method()

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -12,14 +12,28 @@ export class ProductCardComponent implements OnInit {
   @Input('product') product: Product;
   @Input('show-actions') showActions = true;
   @Input('card-width') cardWidth; // TO DO
+  @Input('shopping-cart') shoppingCart
 
   constructor(private cartSerice: ShoppingCartService) { }
 
   ngOnInit() {
   }
 
-  addToCart(product: Product){
-    this.cartSerice.addToCart(product);
+  addToCart(){
+    this.cartSerice.addToCart(this.product);
   }
 
+  decrementQuantity(){
+    this.cartSerice.decrementItemQuantity(this.product);
+  }
+
+  getQuantity(){
+    // returns 0 if there is no  a shopping cart yet
+    if(!this.shoppingCart) return 0;
+
+    // gets the current product object in the shopping cart if any
+    // returns the quanttity of that product if any or 0
+    let item = this.shoppingCart.items[this.product.key];
+    return item ? item.quantity : 0;
+  }
 }
