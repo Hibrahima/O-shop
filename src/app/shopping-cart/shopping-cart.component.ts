9c203a93e8ac4d183e4f72d8e85c7bec9102b018
Hibrahima <haidara99@gmail.com>
2019-01-08 18:40:40 +0100

    - refactored code - added subscription field to unsubscribe when this component is destroyed - implemented onDestroy

--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ShoppingCart } from './../models/shopping-cart';
+import { ShoppingCartService } from './../services/shopping-cart.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html',
   styleUrls: ['./shopping-cart.component.css']
 })
-export class ShoppingCartComponent implements OnInit {
+export class ShoppingCartComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  cart : ShoppingCart;
+  subscription: Subscription;
 
-  ngOnInit() {
+  constructor(private cartService: ShoppingCartService) { }
+
+  async ngOnInit() {
+   this.subscription = (await this.cartService.getCart()).subscribe(cart =>  this.cart = cart);
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
   }
 
 }
